45644e19057c77eeeabfd728b67ea046a564071a
nor <nori@vbminigame.sakura.ne.jp>
2018-12-14 22:43:59 +0900

    first commit

---
+++ src/nori/practice/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package nori.practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
